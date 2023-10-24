d083e446304464955672cf22e1da2b0a0ed819b9
hongpook <hongphuc12@gmail.com>
2023-10-24 11:34:31 +0700

    week 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListStudentComponent } from './list-student/list-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "student", component: ListStudentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
